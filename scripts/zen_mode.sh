1e1c51cc6298fa1e5b31784d73e60724097dcb24
h0useofdupree <joel.riekemann@gmail.com>
2024-03-13 09:06:29 +0100

    fixed zen mode

--- scripts/zen_mode.sh
+++ scripts/zen_mode.sh
@@ -2,16 +2,16 @@
 # Copyright (c) 2023 Joel Riekemann. All Rights Reserved.
 
 
-VARPATH="~/.config/hypr/var/zenmode"
-ZENMODE=$(cat $VARPATH)
+VARPATH="$HOME/.config/hypr/var/zenmode"
+ZENMODE=$(cat "$VARPATH")
 
-if [ $ZENMODE = "0" ] ; then
-  echo "1" > $VARPATH
+if [ "$ZENMODE" = "0" ] ; then
+  echo "1" > "$VARPATH"
   
-  if pidof waybar &>/dev/null; then
+  if pidof waybar >/dev/null; then
     killall waybar
   fi
-  waybar -s ~/.config/waybar/style_zen.css &
+  waybar -s ~/.config/waybar/style_zen.css -c ~/.config/waybar/config_zen &
   
   hyprctl --batch "\
     keyword general:gaps_in 0;\
@@ -19,10 +19,10 @@ if [ $ZENMODE = "0" ] ; then
     keyword decoration:rounding 0;\
     keyword general:col.active_border rgba(FFFFFFAA)"
 
-  notify-send 'ZEN Mode' 'Activated'
+  hyprctl notify 1 2500 "rgba(ffffff99)" "Zen Mode activated" &
   exit
 fi
-echo "0" > $VARPATH
-~/.config/hypr/scripts/restart_waybar.sh &
+echo "0" > "$VARPATH"
+fish -c "restartWaybar"
 hyprctl reload
-notify-send 'ZEN Mode' 'Deactivated'
+hyprctl notify 1 2500 "rgba(ffffff99)" "Zen Mode deactivated" &
